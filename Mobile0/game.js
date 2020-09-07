50d533c49310aee070144ccb87cf431351cc8996
WrityP <35145484+WrityP@users.noreply.github.com>
2020-09-07 21:23:20 +0200

    Update game.js

--- Mobile0/game.js
+++ Mobile0/game.js
@@ -227,8 +227,8 @@ $(document).ready(function(){
 
 	new Character(orc_form);
 	new Character(goblin_form);
-	player.render_log(fill_log_line(defend_strings,player.get_line_object('D')));
-	//player.render_log('"I have finally found you, beast!"');
+	
+	player.render_log('"I have finally found you, beast!"');
 	characters[1].render_log('"Have you come to die as well, human?"');
 	characters[2].render_log('<i>Something hideous jumps out of a nearby bush.</i><br/>"Do you want me to carve her up, boss?"');
 	player.start_turn();
